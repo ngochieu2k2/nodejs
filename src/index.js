d79fc180f5f7aa9504b5d48501e1d78ea4cc7d58
ngochieu2k2 <raysmon46@gmail.com>
2023-11-07 07:46:43 +0700

    commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import path from 'path'
+import express  from 'express'
+import morgan  from 'morgan'
+import { engine } from 'express-handlebars'
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(morgan('combined'))
+
+//template engine 
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './src/resources/view');
+
+app.get('/', (req, res) => {
+     res.render('home');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`)
+})
